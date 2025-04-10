ed7a44f887249863034642f9c3dbe915f5dca527
Renan de Almeida <renan-dealmeida@hotmail.com>
2025-04-10 20:37:21 -0300

    added rotation in axis x, labels and sorted by descend values

--- src/main.py
+++ src/main.py
@@ -1,21 +1,24 @@
 import pandas as pd
-import matplotlib.pyplot as plt  # Corrigido
-import seaborn as sbn
-import numpy as np
+import matplotlib.pyplot as plt
 
 df = pd.read_csv(r'C:\intellij-projects\postech\first-tech-challenge\wine_data\table_wine\dados_2024.csv',
                  encoding="UTF-8", thousands=".", decimal=",")
 
 df["Valor (US$)"] = pd.to_numeric(df["Valor (US$)"], errors="coerce")
-
 df = df.dropna(subset=["Valor (US$)"])
 
-media = df["Valor (US$)"].mean(skipna=True)
+media = df["Valor (US$)"].mean()
+print(f"\nMédia do valor em (US$) é: {media:.2f}\n")
 
-pd.options.display.float_format = "{:.2f}".format
-
-print(f"Média do valor em (US$) é: {media}\n")
+df = df.sort_values(by="Valor (US$)", ascending=False)
 print(df)
 
-df.plot(x="Países", y="Valor (US$)", figsize=(12,12), kind="bar")
-plt.show()  # Exibir gráfico corretamente
+ax = df.plot(x="Países", y="Valor (US$)", figsize=(10, 6), kind="bar")
+plt.ylim(0, 1000)
+plt.xticks(rotation=45)
+ax.tick_params(axis='x', labelsize=10, pad=10)
+ax.set_title("Valor de Exportação de Vinho por País (US$)")
+ax.set_xlabel("Países")
+ax.set_ylabel("Valor (US$)")
+plt.tight_layout()
+plt.show()
