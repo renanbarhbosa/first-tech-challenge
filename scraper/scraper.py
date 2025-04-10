b612f4c1d390cb448b9c0dbc7a503d5dc037c187
Renan de Almeida <renan-dealmeida@hotmail.com>
2025-04-10 10:36:42 -0300

    scraped wine data for another wine types

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -7,28 +7,28 @@ import time
 import os
 
 # Configurar driver e diretório de saída
-chrome_driver_path = r'C:\intellij-projects\postech\tech-challenge-1\chrome-driver\chromedriver-win64\chromedriver.exe'
-output_dir = r'C:\intellij-projects\postech\tech-challenge-1\data'
+chrome_driver_path = r'C:\intellij-projects\postech\first-tech-challenge\chrome_driver\chromedriver-win64\chromedriver.exe'
+output_dir = r'C:\intellij-projects\postech\first-tech-challenge\wine_data\grape_juice'
 
 # Criar diretório se não existir
 os.makedirs(output_dir, exist_ok=True)
-
 service = Service(executable_path=chrome_driver_path)
 driver = webdriver.Chrome(service=service)
-
 base_url = "http://vitibrasil.cnpuv.embrapa.br/index.php"
 anos = range(2024, 2008, -1)
 
 for ano in anos:
+    key_for_table_wine = "01"
+    key_for_foaming_wine = "02"
+    key_for_fresh_grapes = "03"
+    key_for_grape_juice = "04"
     try:
-        driver.get(f"{base_url}?ano={ano}&opcao=opt_06&subopcao=subopt_01")
+        # Para alternar entre os tipos de vinho, apenas alterar o valor da chave &subopcao=subopt_02 ou 3 e 4
+        driver.get(f"{base_url}?ano={ano}&opcao=opt_06&subopcao=subopt_{key_for_grape_juice}")
         time.sleep(2)
 
         # Localizar a tabela principal
-        tabela = driver.find_element(
-            By.XPATH,
-            '/html/body/table[4]/tbody/tr/td[2]/div/div/table[1]'
-        )
+        tabela = driver.find_element(By.XPATH, '/html/body/table[4]/tbody/tr/td[2]/div/div/table[1]')
 
         # Extrair HTML da tabela
         html_tabela = tabela.get_attribute('outerHTML')
@@ -45,12 +45,7 @@ for ano in anos:
         # Criar DataFrame e salvar em CSV
         df = pd.DataFrame(dados)
         csv_path = os.path.join(output_dir, f'dados_{ano}.csv')
-        df.to_csv(
-            csv_path,
-            index=False,
-            header=False,
-            encoding='utf-8'
-        )
+        df.to_csv(csv_path, index=False, header=False, encoding='utf-8')
 
         print(f"Dados de {ano} salvos em {csv_path}")
 
--- src/main.py
+++ src/main.py
@@ -3,7 +3,7 @@ import matplotlib as mpt
 import seaborn as sbn
 import numpy as np
 
-df_2024 = pd.read_csv(r'C:\intellij-projects\postech\first-tech-challenge\wine_data\dados_2024.csv')
+df_2024 = pd.read_csv(r'/wine_data/table_wine/dados_2024.csv')
 print("Head de exportacao 2024")
 print(df_2024.head())
 print("Final do head de exportacao 2024")
