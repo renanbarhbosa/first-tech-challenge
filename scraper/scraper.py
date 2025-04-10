a9714bf3f64f45f47d1a0bb2a7a245f79a3276a2
Renan de Almeida <renan-dealmeida@hotmail.com>
2025-04-10 12:15:36 -0300

    mean

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -6,11 +6,9 @@ import pandas as pd
 import time
 import os
 
-# Configurar driver e diretório de saída
 chrome_driver_path = r'C:\intellij-projects\postech\first-tech-challenge\chrome_driver\chromedriver-win64\chromedriver.exe'
 output_dir = r'C:\intellij-projects\postech\first-tech-challenge\wine_data\grape_juice'
 
-# Criar diretório se não existir
 os.makedirs(output_dir, exist_ok=True)
 service = Service(executable_path=chrome_driver_path)
 driver = webdriver.Chrome(service=service)
@@ -23,26 +21,20 @@ for ano in anos:
     key_for_fresh_grapes = "03"
     key_for_grape_juice = "04"
     try:
-        # Para alternar entre os tipos de vinho, apenas alterar o valor da chave &subopcao=subopt_02 ou 3 e 4
         driver.get(f"{base_url}?ano={ano}&opcao=opt_06&subopcao=subopt_{key_for_grape_juice}")
         time.sleep(2)
 
-        # Localizar a tabela principal
         tabela = driver.find_element(By.XPATH, '/html/body/table[4]/tbody/tr/td[2]/div/div/table[1]')
 
-        # Extrair HTML da tabela
         html_tabela = tabela.get_attribute('outerHTML')
 
-        # Parsear o HTML com BeautifulSoup
         soup = BeautifulSoup(html_tabela, 'html.parser')
         dados = []
 
-        # Extrair linhas e células
         for linha in soup.find_all('tr'):
             celulas = linha.find_all(['th', 'td'])
             dados.append([celula.get_text(strip=True) for celula in celulas])
 
-        # Criar DataFrame e salvar em CSV
         df = pd.DataFrame(dados)
         csv_path = os.path.join(output_dir, f'dados_{ano}.csv')
         df.to_csv(csv_path, index=False, header=False, encoding='utf-8')
--- src/main.py
+++ src/main.py
@@ -1,10 +1,21 @@
 import pandas as pd
-import matplotlib as mpt
-import seaborn as sbn
-import numpy as np
 
-df_2024 = pd.read_csv(r'/wine_data/table_wine/dados_2024.csv')
-print("Head de exportacao 2024")
-print(df_2024.head())
-print("Final do head de exportacao 2024")
+# Carregar o arquivo CSV
+table_wines_2024_raw = pd.read_csv(r'C:\intellij-projects\postech\first-tech-challenge\wine_data\table_wine\dados_2024.csv')
 
+# Exibir informações iniciais
+print("Head de exportação 2024")
+print(table_wines_2024_raw.head())
+print("Final do head de exportação 2024\n")
+
+print("Início info")
+table_wines_2024_raw.info()
+print("Fim info\n")
+
+table_wines_2024_raw["Valor (US$)"] = table_wines_2024_raw["Valor (US$)"].str.replace(r'[^\d.-]', '', regex=True)
+table_wines_2024_raw["Valor (US$)"] = pd.to_numeric(table_wines_2024_raw["Valor (US$)"], errors='coerce')
+table_wines_2024_raw.dropna(subset=["Valor (US$)"], inplace=True)
+media_valor = table_wines_2024_raw["Valor (US$)"].mean()
+print(f"Média do Valor (US$): {media_valor}")
+
+print(table_wines_2024_raw)
