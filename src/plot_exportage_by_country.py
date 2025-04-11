d69b5a8547cd1fa456f66968fd4cdafaba5ff896
Renan de Almeida <renan-dealmeida@hotmail.com>
2025-04-11 17:36:37 -0300

    plot by country table wine ok, plot table wine aggregate 2009-2024 ok

--- src/plot_exportage_by_country.py
+++ src/plot_exportage_by_country.py
@@ -1,9 +1,7 @@
-#TODO - AJUSTAR A ROTAÇÃO COM OS PAÍSES E EVITAR A SOBREPOSIÇÃO
-
 import pandas as pd
 import matplotlib.pyplot as plt
 
-df = pd.read_csv(r'C:\intellij-projects\postech\first-tech-challenge\wine_data\table_wine\dados_2024.csv',
+df = pd.read_csv(r'C:\intellij-projects\postech\first-tech-challenge\wine_exportage_data\table_wine_exportage_data\dados_2024.csv',
                  encoding="UTF-8", thousands=".", decimal=",")
 
 df["Valor (US$)"] = pd.to_numeric(df["Valor (US$)"], errors="coerce")
@@ -18,7 +16,7 @@ print(df)
 ax = df.plot(x="Países", y="Valor (US$)", figsize=(10, 6), kind="bar")
 plt.ylim(0, 1000)
 plt.xticks(rotation=45)
-ax.tick_params(axis='x', labelsize=10, pad=10)
+ax.tick_params(axis='x', labelsize=3.5, pad=3.5)
 ax.set_title("Valor de Exportação de Vinho por País (US$)")
 ax.set_xlabel("Países")
 ax.set_ylabel("Valor (US$)")
---
+++ src/plot_exportage_table_wine_2009_2024.py
@@ -0,0 +1,48 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+dfs = []
+
+for year in range(2009, 2024):
+    try:
+        df_year = pd.read_csv(
+            f'C:\\intellij-projects\\postech\\first-tech-challenge\\wine_exportage_data\\table_wine_exportage_data\\dados_{year}.csv',
+            encoding="UTF-8", thousands=".", decimal=","
+        )
+
+        df_year["Valor (US$)"] = pd.to_numeric(df_year["Valor (US$)"], errors="coerce")
+        df_year["Quantidade (Kg)"] = pd.to_numeric(df_year["Quantidade (Kg)"], errors="coerce")
+
+        df_year = df_year.dropna(subset=["Valor (US$)", "Quantidade (Kg)"])
+
+        df_year["Ano"] = year
+        dfs.append(df_year)
+
+    except FileNotFoundError:
+        print(f"Dados de {year} não encontrados")
+
+df_completo = pd.concat(dfs, ignore_index=True)
+
+df_anual = df_completo.groupby('Ano').agg({'Valor (US$)': 'sum', 'Quantidade (Kg)': 'sum'}).reset_index()
+
+df_anual['Valor (US$)'] = df_anual['Valor (US$)'].apply(lambda x: f'US$ {x:,.2f}')
+df_anual['Quantidade (Kg)'] = df_anual['Quantidade (Kg)'].apply(lambda x: f'{x:,.0f} Kg')
+
+print("\nTabela Anual Consolidada:")
+print(df_anual.to_string(index=False))
+
+plt.figure(figsize=(12, 6))
+
+df_completo.groupby('Ano')[['Valor (US$)', 'Quantidade (Kg)']].sum().plot(
+    kind='line',
+    marker='o',
+    secondary_y='Quantidade (Kg)',
+    title='Exportação de Vinho por Ano'
+)
+
+plt.xlabel('Ano')
+plt.ylabel('Valor (US$)')
+#plt.right_ylabel('Quantidade (Kg)')
+plt.grid(True)
+plt.tight_layout()
+plt.show()
